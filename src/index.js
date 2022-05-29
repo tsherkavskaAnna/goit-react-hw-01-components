4cfdbc43f2c2660f008cf633afccef2b0c5b252b
Anna Tsherkavska <katotoka1985@gmail.com>
2022-05-29 14:20:25 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { ReactDOM } from 'react';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.querySelector(`#root`)
 );
