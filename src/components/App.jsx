50c56d5ef13b18760b25062c372fba87944fa466
Anna Tsherkavska <katotoka1985@gmail.com>
2022-05-29 19:06:53 +0200

    task2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import Profile from './Profile/Profile';
 import user from '../data/user.json';
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
@@ -22,6 +24,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import s from './Statistics.module.css';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+
+      <ul className={s.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li key={id} className={s.item}>
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+React.PropTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default Statistics;
